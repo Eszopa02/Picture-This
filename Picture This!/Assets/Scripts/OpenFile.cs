395cfdcc29f4d2ac109c80bf2cd4f675bdcda45b
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2025-01-09 16:57:34 -0600

    Activate Open File Button

---
+++ Picture This!/Assets/Scripts/OpenFile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenFile : MonoBehaviour
+{
+    public GameObject file;
+
+    public void whenButtonClicked()
+    {
+        if (file.activeInHierarchy == true)
+            file.SetActive(false);
+        else
+            file.SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Picture This!/Assets/Scripts/SpriteButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteButton : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("Button Pressed");
+    }
+}
