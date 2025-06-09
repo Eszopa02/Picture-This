6fefa1e01760ef13050a5c72b3a58b6b451883cc
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2025-06-08 21:48:52 -0500

    Return Button Functionality

---
+++ Picture This!/Assets/Scripts/ScrapbookReturnButton.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrapbookReturnButton : MonoBehaviour
+{
+    public GameObject FlowerOptions;
+    public GameObject PatternOptions;
+    public GameObject SpotsOptions;
+    public GameObject StripesOptions;
+    public GameObject ColorfulOptions;
+
+    public GameObject PurpleFlower;
+    public GameObject PurplePattern;
+    public GameObject PurpleSpots;
+    public GameObject PurpleStripes;
+    public GameObject Lilac;
+
+    public void DeactivateButtons()
+    {
+        FlowerOptions.SetActive(false);
+        PatternOptions.SetActive(false);
+        SpotsOptions.SetActive(false);
+        StripesOptions.SetActive(false);
+        ColorfulOptions.SetActive(false);
+    }
+
+    public void ActivateButtons()
+    {
+        PurpleFlower.SetActive(true);
+        PurplePattern.SetActive(true);
+        PurpleSpots.SetActive(true);
+        PurpleStripes.SetActive(true);
+        Lilac.SetActive(true);
+    }
+
+    public void Return()
+    {
+        DeactivateButtons();
+        ActivateButtons();
+    }
+}
