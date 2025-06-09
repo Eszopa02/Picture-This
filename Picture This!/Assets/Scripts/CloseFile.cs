b4d66ec1762302685fcbcfe18d96715d572dd2b7
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2025-06-09 00:23:59 -0500

    Select Background Color/Pattern

---
+++ Picture This!/Assets/Scripts/CloseFile.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseFile : MonoBehaviour
+{
+    public GameObject file;
+
+    /*public GameObject ColorfulPaperBlue;
+    public GameObject ColorfulPaperPink;
+
+    public GameObject FlowerOptions;
+    public GameObject PatternOptions;
+    public GameObject SpotsOptions;
+    public GameObject StripesOptions;
+
+    public GameObject PurpleSpotsPaper;
+    public GameObject PurpleStripesPaper;
+    public GameObject PurpleFlowerPaper;
+    public GameObject PurplePatternPaper;
+    public GameObject LilacPaper;
+    public GameObject PurpleFlowerCanvas;
+
+
+    public GameObject PaperOptions;*/
+
+
+
+    public void whenButtonClicked()
+    {
+        if (file.activeInHierarchy == true)
+        {
+            file.SetActive(false);
+
+            /*PurpleSpotsPaper.SetActive(true);
+            PurpleStripesPaper.SetActive(true);
+            PurplePatternPaper.SetActive(true);
+            PurpleFlowerPaper.SetActive(true);
+            LilacPaper.SetActive(true);
+
+
+            ColorfulPaperBlue.SetActive(false);
+            ColorfulPaperPink.SetActive(false);
+
+            FlowerOptions.SetActive(false);
+            PatternOptions.SetActive(false);
+            SpotsOptions.SetActive(false);
+            StripesOptions.SetActive(false); */
+        }
+         /*else
+        {
+            file.SetActive(true);
+
+            ColorfulPaperBlue.SetActive(false);
+            ColorfulPaperPink.SetActive(false);
+
+            FlowerOptions.SetActive(false);
+            PatternOptions.SetActive(false);
+            SpotsOptions.SetActive(false);
+            StripesOptions.SetActive(false);
+        }
+         */
+
+
+
+    }
+}
