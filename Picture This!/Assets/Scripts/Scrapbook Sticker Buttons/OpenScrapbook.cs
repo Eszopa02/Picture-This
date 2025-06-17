017f948ba23e7806751f412e08381a958ea12e2e
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2025-06-17 00:43:07 -0500

    Open Scrapbook

--- Picture This!/Assets/Scripts/Scrapbook Sticker Buttons/OpenScrapbook.cs
+++ Picture This!/Assets/Scripts/Scrapbook Sticker Buttons/OpenScrapbook.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class OpenScrapbook : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Scrapbook; 
 
-    // Update is called once per frame
-    void Update()
+    public void WhenButtonClicked()
     {
         
+          Scrapbook.SetActive(true);
+
+
+
     }
 }
