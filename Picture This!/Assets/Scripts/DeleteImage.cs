54b18c6555f8ccde0191bdee78d56ed1b9258f91
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2025-07-17 22:29:16 -0500

    Fixed Issue where spawned stickers appeared above the sticker book

--- Picture This!/Assets/Scripts/DeleteImage.cs
+++ Picture This!/Assets/Scripts/DeleteImage.cs
@@ -6,18 +6,31 @@ using UnityEngine.UI;
 
 public class DeleteImage : MonoBehaviour, IDropHandler
 {
+    //public GameObject OpenTrash;
+    //public GameObject ClosedTrash;
+
     public void OnDrop(PointerEventData eventData)
     {
+        //OpenTrash.SetActive(true);
+        
         if (eventData.pointerDrag != null)
         {
             DragImage droppedObject = eventData.pointerDrag.GetComponent<DragImage>();
 
+            //OpenTrash.SetActive(false);
+
             if (droppedObject != null)
             {
+                //OpenTrash.SetActive(true);
 
                 Destroy(droppedObject.gameObject); //
                 Debug.Log("Prefab destroyed upon dropping on the image!");
+
+
+                //OpenTrash.SetActive(true);
+                //ClosedTrash.SetActive(false);
             }
+            
         }
 
 
