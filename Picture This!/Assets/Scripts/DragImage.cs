2a92a62e1cc9a3e79fbca4fb551ab33aed0c96b9
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2025-07-17 19:58:49 -0500

    JJHead Spawns and Moves Correctly
    Need to fix to make sure it's at exact mouse point but it's good enough for now

--- Picture This!/Assets/Scripts/DragImage.cs
+++ Picture This!/Assets/Scripts/DragImage.cs
@@ -2,29 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class DragImage : MonoBehaviour, IDragHandler
+public class DragImage : MonoBehaviour, IPointerDownHandler, IDragHandler, IEndDragHandler
 {
     private Vector2 mousePosition = new Vector2();
     private Vector2 startPosition = new Vector2();
     private Vector2 differencePoint = new Vector2();
+    public RectTransform imageTransform;
+    public Camera mainCamera;
 
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            UpdateMousePosition();
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            UpdateStartPosition();
-            UpdateDifferencePoint();
-        }
+        //if(Input.GetMouseButton(0))
+        //{
+            //UpdateMousePosition();
+        //}
+        //if (Input.GetMouseButtonDown(0))
+        //{
+            //UpdateStartPosition();
+            //UpdateDifferencePoint();
+        //}
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Image pressed!");
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = mousePosition - differencePoint;
+        Debug.Log("Image dragged!");
+
+        //transform.position = mousePosition - differencePoint;
+
+        Vector3 mousePos = Input.mousePosition;
+
+        Vector3 worldPos = mainCamera.ScreenToWorldPoint(mousePos);
+        worldPos.z = 0;
+
+        imageTransform.position = worldPos;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("Image dropped!");
     }
 
     private void UpdateMousePosition()
